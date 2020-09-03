9096c0ba48eb1730072d43980040d6843cf8fa44
vagrant <vagrant@localhost.localdomain>
2020-09-03 08:50:06 +0000

    [Add] function:form

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,6 @@
 class BooksController < ApplicationController
   def index
+  	@book = Book.new
   end
 
   def show
@@ -9,8 +10,17 @@ class BooksController < ApplicationController
   end
 
   def create
+  	book = Book.new(book_params)
+  	book.save
+  	redirect_to books_path
   end
 
   def edit
   end
+
+  private
+  def book_params
+  	params.require(:book).permit(:title, :body)
+  end
+
 end
