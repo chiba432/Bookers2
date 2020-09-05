50b5a8dca6a61fd7c5770fe78b87359ea1807d50
vagrant <vagrant@localhost.localdomain>
2020-09-05 06:20:58 +0000

    [Add] error message(create)

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -13,9 +13,13 @@ class BooksController < ApplicationController
   end
 
   def create
-  	book = Book.new(book_params)
-  	book.save
-  	redirect_to books_path
+  	@book = Book.new(book_params)
+  	if @book.save
+  	  redirect_to books_path, notice: "You have created book successfully."
+    else
+      @books = Book.all
+      render :index
+    end
   end
 
   def edit
@@ -25,7 +29,7 @@ class BooksController < ApplicationController
   def update
     book = Book.find(params[:id])
     book.update(book_params)
-    redirect_to book_path(book)
+    redirect_to book_path(book), notice: "You have updated book successfully."
   end
 
   def destroy
